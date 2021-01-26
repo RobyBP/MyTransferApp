f6a4c43858d15796fa53d53da03b6d2d64273dab
RobyBP <robert.baricevicpetrus@gmail.com>
2021-01-26 20:48:03 +0100

    Added a database

---
+++ app/src/main/java/com/robybp/mytransferapp/dao/GuestBookDao.kt
@@ -0,0 +1,42 @@
+package com.robybp.mytransferapp.dao
+
+import androidx.room.*
+import com.robybp.mytransferapp.models.datamodels.Driver
+import com.robybp.mytransferapp.models.datamodels.Guest
+import io.reactivex.Flowable
+import io.reactivex.Single
+
+@Dao
+interface GuestBookDao {
+
+    @Query("SELECT * FROM guests ORDER BY dateOfArrival DESC")
+    fun getAllGuests() : Flowable<List<Guest>>
+
+    @Query("SELECT * FROM drivers")
+    fun getAllDrivers(): Flowable<List<Driver>>
+
+    @Query("SELECT * FROM guests WHERE guestId = :id")
+    fun getGuest(id: Int) : Single<Guest>
+
+    @Query("SELECT * FROM drivers WHERE name = :name")
+    fun getDriver(name: String) : Single<Driver>
+
+    @Insert
+    suspend fun addGuest(guest: Guest)
+
+    @Insert
+    suspend fun addDriver(driver: Driver)
+
+    @Delete
+    suspend fun deleteGuest(guest: Guest)
+
+    @Delete
+    suspend fun deleteDriver(driver: Driver)
+
+    @Update
+    suspend fun updateGuestData(guest: Guest)
+
+    @Update
+    suspend fun updateDriverData(driver: Driver)
+
+}
