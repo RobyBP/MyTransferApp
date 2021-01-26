bbe993da05d7f89892636e1dc0c6c6ec591370f4
RobyBP <robert.baricevicpetrus@gmail.com>
2021-01-26 20:25:00 +0100

    Added data models Guest.kt and Driver.kt

---
+++ app/src/main/java/com/robybp/mytransferapp/models/viewmodels/DriversMenuViewModel.kt
@@ -0,0 +1,7 @@
+package com.robybp.mytransferapp.models.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class DriversMenuViewModel(application: Application): AndroidViewModel(application) {
+}
---
+++ app/src/main/java/com/robybp/mytransferapp/models/viewmodels/HomeScreenViewModel.kt
@@ -0,0 +1,7 @@
+package com.robybp.mytransferapp.models.viewmodels
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class HomeScreenViewModel(application: Application): AndroidViewModel(application) {
+}
